d35d5ce880299d9065b90b1a5d8ed7f2182c75ac
fsalina <fsalina@mobeats.io>
2025-10-04 19:35:31 -0300

    cambio cositas de env

--- qr-app/src/firebase.js
+++ qr-app/src/firebase.js
@@ -1,21 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyABjhAupu_ChbvGwEs0V7HEFSPV0wh1zu4",
   authDomain: "control53.firebaseapp.com",
   projectId: "control53",
   storageBucket: "control53.firebasestorage.app",
   messagingSenderId: "810103553309",
-  appId: "1:810103553309:web:e81395b7e651ee72a8b5f4",
-  measurementId: "G-LXRXD3H3EJ",
+  appId: "1:810103553309:web:b137b2d336595e7aa8b5f4",
+  measurementId: "G-4XGRFGJNY4",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
